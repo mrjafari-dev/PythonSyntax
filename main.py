a899a153c82cc4f5e89873ecc2c7b2378df4d2a0
mrjafarii <mrjafari.dev@Gmail.com>
2022-12-26 15:05:20 -0500

    day 1

---
+++ main.py
@@ -0,0 +1,71 @@
+#----------------------------------------------------------------------------------------------------------------
+#Day 1 **********************************************************************************************************
+#----------------------------------------------------------------------------------------------------------------
+name = "test"
+print('hello mohammad' + name)
+
+# number1 = number1 +2
+
+#fundamental data types
+
+int 
+float 
+bool
+str
+list
+tuple
+set
+dict
+
+print(type(2+2))
+print(2**3)
+print(6 // 4)
+print(6%4)
+number = 5
+number += number
+print(number)
+
+
+# we have long string in python 
+# for example we can used ''' three one quat
+
+longString = '''
+ok we have long text 
+
+''' 
+print(longString)
+
+#start in python basic 14
+
+number1 = 1
+strnum = str(number1)
+typeOfvar = type(strnum)
+#print(typeOfvar)
+
+weather = "\t it\'s \"kind of\" sunny \n hope have god day!"
+#print(weather)
+
+
+#format string
+name  = " mohammadreza "
+age = 22
+mytext = f"my name is {name} and i  {age} years old"
+#print(mytext)
+
+testFormat = "hi  name is {} and i {} years old ".format("alireza" , 17)
+print(testFormat)
+
+
+print(bin(5))
+name = "mohammadrza"
+print(name)
+family,age = "ali ",21
+test = null
+print(family)
+print(age)
+print(test)
+
+#----------------------------------------------------------------------------------------------------------------
+#Day 2 **********************************************************************************************************
+#----------------------------------------------------------------------------------------------------------------
+
