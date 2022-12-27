661fbf57c23dbfa52be2014769c21f4f8442cefa
mrjafarii <mrjafari.dev@Gmail.com>
2022-12-26 23:52:13 -0500

    day 2

---
+++ main_2.py
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------------------------------------------------------
+#Day 2 **********************************************************************************************************
+#----------------------------------------------------------------------------------------------------------------
+
+print("helloWorld")
+# cast in python 
+
+
+str_number = "100"
+number = int(str_number)
+print(type(number))
+str_number = str(number)
+print(type(str_number))
+
+#scape Sequence
+
+weather = "\t it's \"kind of\"sunny \n my new text about weather"
+print(weather)
+
+#Crop text (start : stop : StepOver)
+
+selfish = "0123456789"
+
+print(selfish[4:9:2])
+print(selfish[::2])
+print(selfish[-1])
+print(selfish[::-1])
+
+#start in 19
