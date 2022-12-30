72090de80699aa52ace23faef0c7680f0b661009
mrjafarii <mrjafari.dev@Gmail.com>
2022-12-30 00:28:42 -0500

    day 5 part 2

--- main-5.py
+++ main-5.py
@@ -28,3 +28,12 @@ test = ' '
 newTest = test.join(['my','name','is','mohammadreza'])
 print(test)
 print(newTest)
+
+#list Unpacking 
+a,b,c, *others , d= ['1',2,'3',4,5,6,7,8,9]
+
+print(a)
+print(b)
+print(c)
+print(others)
+print(d)
