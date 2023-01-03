f52b6870ce973d6e3de895ce4845a2460a80c928
mrjafarii <mrjafari.dev@Gmail.com>
2023-01-04 02:11:42 +0330

    day 6 part 2

--- main-6.py
+++ main-6.py
@@ -40,6 +40,26 @@ print(sampleList[1]['a'][2])
 print(dictionary.get('x',22))
 user = dict(name = 'ali',family = 'rezayii')
 print(user.get('name'))
+print(1 in dictionary.items())
 
+#copy value of user in another of ram adress
+user2= user.copy()
+#add value to dictionary
+user2.update({'age':22})
+#remove end value 
+user2.popitem()
+#Remove value
+user2.pop('name')
+print(user2)
 
+#tuples like to arrays but inmutable 
 
+tpl = (0,1,2,3)
+print(tpl)
+
+#set in phyton remove duplicate values 
+
+setValue = {1,2,3,4,5,6,6,6,}
+testList=[1,2,3,4,5,5,5,5]
+print(setValue)
+print(set(testList))
