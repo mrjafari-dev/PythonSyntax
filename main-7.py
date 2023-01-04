518c9a1ef13eb39765412045bf78e0e9a1f147af
mrjafarii <mrjafari.dev@Gmail.com>
2023-01-04 16:13:11 +0330

    day 7 part 1

---
+++ main-7.py
@@ -0,0 +1,32 @@
+#----------------------------------------------------------------------------------------------------------------
+#Day 7 **********************************************************************************************************
+#----------------------------------------------------------------------------------------------------------------
+
+#sets 
+my_set = {1,2,3,4,5,6,7,8,9}
+your_set = {1,2,3}
+#print(my_set.difference(your_set))
+#my_set.discard(9)
+#print(my_set)
+#my_set.difference_update(your_set)
+#print(my_set)
+print(my_set.intersection(your_set))
+
+
+
+#if
+
+is_old = True
+is_licenced = True
+
+if is_old :
+    print('cant drive car becus you are old')
+    print('cant drive car becus you are old')
+elif is_licenced:
+    print('you can drive now!')
+else:
+    print("you are not of age")
+
+
+if is_licenced and is_old:print("two value is true")
+
